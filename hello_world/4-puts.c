586c3314caf768670652ca398d7e144fb2e25f0a
noemiepham <huynhtieuquy@gmail.com>
2022-06-06 14:53:53 +0200

    put function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/* initializing strings */
-	char str1[] = "\"Programming is like building a multilingual puzzle";
-	/*  writing to stdout */
-	puts(str1);
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
