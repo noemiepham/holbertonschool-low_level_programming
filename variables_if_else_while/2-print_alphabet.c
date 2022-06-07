164dc4c199163db1bcace90fd5938a71fab516d8
noemiepham <huynhtieuquy@gmail.com>
2022-06-07 12:57:32 +0200

    the alphabet game

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
+int main(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+	write(1, "\n", 1);
+	return (0);
+}
