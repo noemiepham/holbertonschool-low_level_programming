f2537bbe85bbd7386e70abfb5f96eb9440fb9d17
noemiepham <huynhtieuquy@gmail.com>
2022-06-07 13:46:11 +0200

    alphaBet upper lower

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
+int main(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+	write(1, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 26);
+	write(1, "\n", 1);
+	return (0);
+}
