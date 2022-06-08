540e13b6c9e5a3b5b3951d31ef8a393554b1b744
noemiepham <huynhtieuquy@gmail.com>
2022-06-08 21:54:17 +0200

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,8 @@
 #include "main.h"
 #include <stdlib.h>
 #include <time.h>
-
+#include <unistd.h>
+#include <stdio.h>
 /**
  * print_last_digit - print the last digit of a number
  * @n: the number to be checked
@@ -17,9 +18,13 @@
 int print_last_digit(int n)
 
 {
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
 	n = n % 10;
+	if (n < 0)
+	{
+		n = n * (-1);
+		_putchar(n + '0');
+		return (n);
+	}
+	_putchar(n + '0');
 	return (n);
 }
