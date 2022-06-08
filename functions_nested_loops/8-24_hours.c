4691366477db0e0d3c9ae32a5b660060d3e2da2a
noemiepham <huynhtieuquy@gmail.com>
2022-06-09 00:16:02 +0200

    24 heurs

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,38 +12,30 @@
 
 void jack_bauer(void)
 {
+	int h;
+	int m;
 	int h1;
 	int h2;
 	int m1;
 	int m2;
 
-	h1 = 0;
-	while (h1 < 3)
-	{
-		h2 = 0;
-		while (h2 <= 9)
+	h = 0;
+	while (h < 24)
+	{	h1 = h / 10;
+		h2 = h % 10;
+		m = 0;
+		while (m < 60)
 		{
-			if (h1 == 2 && h2 < 4)
-			{
-				m1 = 0;
-				while (m1 < 6)
-				{
-					m2 = 0;
-					while (m2 <= 9)
-					{
-						_putchar(h1 + '0');
-						_putchar(h2 + '0');
-						_putchar(':');
-						_putchar(m1 + '0');
-						_putchar(m2 + '0');
-						_putchar('\n');
-					m2++;
-					};
-				m1++;
-				}
-			}
-		h2++;
+			m1 = m / 10;
+			m2 = m % 10;
+			_putchar(h1 + '0');
+			_putchar(h2 + '0');
+			_putchar(':');
+			_putchar(m1 + '0');
+			_putchar(m2 + '0');
+			_putchar('\n');
+		m++;
 		}
-	h1++;
+	h++;
 	}
 }
