0fc79b47ea2d69dfc1a74cfaf488fa56d34237de
noemiepham <huynhtieuquy@gmail.com>
2022-06-08 16:00:01 +0200

    halphabe

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,30 +1,20 @@
-/*
-* File: 2. 10 x alphabet
-* Auth: NoémePHAM
-*/
-
-#include "main.h"
-
+#include <unistd.h>
 /**
- * print_alphabet_x10, - function that prints 10 itimes the alphabet
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
-void print_alphabet_x10(void)
+int main(void)
 {
 	int i;
 
 	i = 0;
 	while (i < 10)
 	{
-		char letter;
-
-		letter = 'a';
-		while (letter <= 'z')
-		{
-			_putchar(letter);
-			letter++;
-		}
-	_putchar('\n');
-	i++;
-	}
+		write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+		write(1, "\n", 1);
+		i++;
+	};
+return (0);
 }
