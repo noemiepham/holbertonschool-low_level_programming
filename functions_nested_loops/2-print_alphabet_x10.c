b82bc7a881d9b72ebf58fc59b1df3a1672e496d7
noemiepham <huynhtieuquy@gmail.com>
2022-06-08 16:14:32 +0200

    add alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,30 @@
-#include <unistd.h>
+/*
+* File: 2. 10 x alphabet
+* Auth: NoémePHAM
+*/
+
+#include "main.h"
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_alphabet_x10 - function that prints 10 itimes the alphabet
  */
 
-int main(void)
+void print_alphabet_x10(void)
 {
 	int i;
 
 	i = 0;
 	while (i < 10)
 	{
-		write(1, "abcdefghijklmnopqrstuvwxyz", 26);
-		write(1, "\n", 1);
-		i++;
-	};
-return (0);
+		char letter;
+
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	_putchar('\n');
+	i++;
+	}
 }
