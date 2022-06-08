4ea0cda2d734589070359a63d56f0b06d822b160
noemiepham <huynhtieuquy@gmail.com>
2022-06-09 01:19:31 +0200

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+/*
+* File: 10 a + b
+* Auth: NoémePHAM
+*/
+
+#include "main.h"
+#include <unistd.h>
+
+/**
+* add - Add two integers.
+* @a : numero;
+* @b : numero;
+*
+* Return: The result of the addition
+*/
+
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
