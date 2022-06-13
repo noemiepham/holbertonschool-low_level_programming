cb9d2d7eb2ee4bb0335ac75d14b51d743c5d38a5
noemiepham <huynhtieuquy@gmail.com>
2022-06-13 09:57:59 +0200

    multilicatione two number

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "stdio.h"
+/*
+* File: 2. Collaboration is multiplication
+* Auth: NoémePHAM
+*/
+
+/**
+ * mul - Write a function that multiplies two integers..
+ * @a: number
+ * @b: number
+ * Return: Alway 0.
+ */
+
+int mul(int a, int b)
+{
+	a = a * b;
+	return (a);
+}
