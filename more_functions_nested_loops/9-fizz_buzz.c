d736799f4543eb97bfa9acac93915247b48f29f7
noemiepham <huynhtieuquy@gmail.com>
2022-06-13 22:17:39 +0200

    FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -23,8 +23,8 @@ int main(void)
 		printf("Fizz");
 		else if ((n % 5) == 0)
 		printf("Buzz");
-		else if ((n % 3 == 0) && (n % 5 == 0))
-		printf("BuzzFizz");
+		else if ((n % 15 == 0)
+		printf("FizzBuzz");
 		else
 			printf("%d", n);
 		if (n < 100)
