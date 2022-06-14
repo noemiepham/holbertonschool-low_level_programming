5d4365785c164c5ee6b05774b87b1d9df2db510d
noemiepham <huynhtieuquy@gmail.com>
2022-06-14 22:08:19 +0200

    put odd number

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+/*
+*6. Half the lies they tell about me aren't true
+* Auth: NoémePHAM
+*/
+
+#include "main.h"
+
+/**
+* puts - Write a function that prints every other character of a string
+* @str : string
+*/
+
+void puts2(char *str)
+{
+	int i; 
+
+	i = 0;
+	while (str[i] != 0)
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+	i++;
+	}
+_putchar('\n');
+}
+
