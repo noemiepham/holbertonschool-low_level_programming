bacf82968b967001b79cc47293389a1ab3abb13f
noemiepham <huynhtieuquy@gmail.com>
2022-06-14 09:11:19 +0200

    pointer

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/*
+*0. 98 Battery st.
+* Auth: NoémePHAM
+*/
+
+#include "main.h"
+
+/**
+* reset_to_98 - a pointer to an int as parameter and updates the value;
+* @n: pointer;
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
