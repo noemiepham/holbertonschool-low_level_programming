c1f66ccd4e82852adc9f33f4084e7b84304db88b
noemiepham <huynhtieuquy@gmail.com>
2022-06-15 15:55:53 +0200

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+/*
+* 8. Arrays are not pointers
+* Auth: NoémePHAM
+*/
+
+#include <stdio.h>
+/**
+* print_array - print elements of an array;
+* @a: pointer;
+* @n: size of array,
+*/
+
+void print_array(int *a, int n)
+{
+	int i; 
+
+	i = 0;
+	while (i < n)
+	{
+		if (i != n - 1)
+		printf("%d, ", a[i]);
+		else
+		printf("%d", a[i]);
+	i++;
+	}
+printf("\n");
+}
