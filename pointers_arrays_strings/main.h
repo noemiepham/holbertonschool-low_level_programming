8128ab6dd9277a27ad1ea25daba3a99d0b5b47ac
noemiepham <huynhtieuquy@gmail.com>
2022-06-18 10:52:25 +0200

    add more function

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -42,5 +42,15 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
+/*
+*more_pointer_array_string
+*/
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
 #endif
