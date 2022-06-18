c6af27c7b23dbeb1dcb19cb78f905f496079a82f
noemiepham <huynhtieuquy@gmail.com>
2022-06-18 23:00:42 +0200

    compaire 2 string

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,20 +8,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+		unsigned int bytes = 0;
 		int i;
-		int bytes = 0;
 
-	while (*accept)
+	while (*s)
 	{
-		for (i = 0; s[i]; i++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (*accept ==  s[i])
+			if (*s ==  accept[i])
 			{
 				bytes++;
 				break;
 			}
+			else if (accept[i + 1] == '\0')
+				return (bytes);
 		}
-	accept++;
+	s++;
 	}
-	return (bytes + 1);
+	return (bytes);
 }
