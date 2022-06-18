89b8cfeabbbb55db4284e6651e35beea9d6537ac
noemiepham <huynhtieuquy@gmail.com>
2022-06-18 09:09:38 +0200

    string upper case

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* *cap_string - newline and upper case
+* @str : string
+* Return: str
+*/
+
+char *cap_string(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' ||
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' ||
+			str[i - 1] == ';' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == '!' ||
+			str[i - 1] == '?' ||
+			str[i - 1] == '"' ||
+			str[i - 1] == '(' ||
+			str[i - 1] == ')' ||
+			str[i - 1] == '{' ||
+			str[i - 1] == '}' ||
+			i == 0)
+			str[i] -= 32;
+		i++;
+	}
+return (str);
+}
