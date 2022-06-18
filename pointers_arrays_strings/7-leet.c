c279b48a8322926cff0b24ef3c0c4a11f49da766
noemiepham <huynhtieuquy@gmail.com>
2022-06-18 11:08:12 +0200

    change lette to number

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -18,8 +18,8 @@ char *leet(char *str)
 		while (j <= 8)
 		{
 			if (str[i] == leet[j] ||
-				str[i] - 32 == leet[j])
-					str[i] = j + '0';
+str[i] - 32 == leet[j])
+				str[i] = j + '0';
 				j++;
 		}
 	i++;
