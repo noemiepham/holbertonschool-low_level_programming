1a88ddd2b6ed200362fd6472ec36bd0519616d95
noemiepham <huynhtieuquy@gmail.com>
2022-06-20 09:09:44 +0200

    recursion putchar

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,13 +7,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-_putchar('\n');
+	if (*s == '\0')
+		{
+			_putchar('\n');
+			return; 
+		}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
