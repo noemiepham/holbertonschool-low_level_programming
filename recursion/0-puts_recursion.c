e1a362ea82949965513f48433d197d6660323dea
noemiepham <huynhtieuquy@gmail.com>
2022-06-20 07:53:46 +0200

    put recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_puts_recursion - put reursion
+* @s: pointer;
+*/
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+_putchar('\n');
+}
---
+++ recursion/main.h
@@ -0,0 +1,25 @@
+#ifndef _MAIN_H_  /* Include guard */
+#define _ MAIN_H_
+
+/**
+* Function-loop
+*/
+
+int  _putchar(char c);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+
+/**
+* Recursion
+*/
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+
+
+
+#endif
