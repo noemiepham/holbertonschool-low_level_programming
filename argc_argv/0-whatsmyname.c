c3bb87f47d3d95c687a559484ac63b4ad54e7aaa
noemiepham <huynhtieuquy@gmail.com>
2022-06-21 11:56:39 +0200

    change name argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc : length of arry argv
+ * @argv : pointer to array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s \n", argv[i]);
+	i++;
+	}
+}
