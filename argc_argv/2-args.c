4403651ccf6bd9bcc56143cc292664263b080fdf
noemiepham <huynhtieuquy@gmail.com>
2022-06-21 13:25:52 +0200

    rogram that prints all arguments it receives.

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc : length of arry argv
+ * @argv : pointer to array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		if (argc != '\0')
+		printf("%s\n", argv[i]);
+	i++;
+	}
+return (0);
+}
