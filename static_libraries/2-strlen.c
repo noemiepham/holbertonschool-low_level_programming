8e7e9bc3d2323b94d12cd521cc85f162e64c05dd
noemiepham <huynhtieuquy@gmail.com>
2022-06-21 11:29:42 +0200

    add new libraries

---
+++ static_libraries/2-strlen.c
@@ -0,0 +1,24 @@
+/*
+* 2. This report, by its very length,
+* Auth: NoémePHAM
+*/
+
+#include "main.h"
+
+/**
+* _strlen - length of string;
+* @s : pointer
+* Return: _strlen
+*/
+
+int _strlen(char *s)
+{
+	int strlen;
+
+	strlen = 0;
+	while (s[strlen] != '\0')
+	{
+		strlen++;
+	}
+return (strlen);
+}
