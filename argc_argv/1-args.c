296846b5cd8506448960e2c5f7866c407a6cb3be
noemiepham <huynhtieuquy@gmail.com>
2022-06-21 13:10:05 +0200

    argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc : length of arry argv
+ * @argv : pointer to array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
