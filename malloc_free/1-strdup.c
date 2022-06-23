e3d274a29566e87600825a90792707158df96055
noemiepham <huynhtieuquy@gmail.com>
2022-06-23 09:29:20 +0200

    strdup coppy string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* char *_strdup - string copy;
+* @str : string
+* Return: Null if malloc is Null
+*/
+
+char *_strdup(char *str)
+{
+	int i, len;
+	char *dest;
+
+	len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	dest = (char *)malloc(sizeof(*str) * (len + 1));
+	i = 0;
+	while (i < len)
+	{
+		dest[i] = str[i];
+	i++;
+	}
+return (dest);
+}
