3076b528fdb442eaab2167ecc97c5956957da234
noemiepham <huynhtieuquy@gmail.com>
2022-06-27 14:05:23 +0200

    fixed error

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,7 +18,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
+			;
 	for (j = 0; s2[j] != '\0'; j++)
+			;
 	if (n > j)
 		array = malloc((i + j + 1) * sizeof(char));
 	else
