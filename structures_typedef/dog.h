1a1723436c45fc13dc5139a02e671cf710e52f77
noemiepham <huynhtieuquy@gmail.com>
2022-06-28 10:42:43 +0200

    add new file header DOG-H

---
+++ structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - define new type
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * u_int - Typedef for unsigned int
+ */
+typedef unsigned int u_int;
+
+#endif
