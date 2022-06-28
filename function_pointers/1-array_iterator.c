758119a23d2cca2ddcb8196abe86c1e2c3a66ed3
noemiepham <huynhtieuquy@gmail.com>
2022-06-28 16:14:25 +0200

    fix code array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,13 +11,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-	int result;
 
-	if (array != NULL && actioon != NULL)
+	if (array != NULL && action != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
-			result = action(array[i]);
+			action(array[i]);
 		}
 	}
 }
