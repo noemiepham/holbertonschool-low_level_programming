229443599829a9695635b0c9b1cbad1620157be6
noemiepham <huynhtieuquy@gmail.com>
2022-06-29 10:33:45 +0200

    coriger betty

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
 #include "3-calc.h"
 
-int op_add(int a, int b) {return (a + b);}
-int op_sub(int a, int b) {return (a - b);}
-int op_mul(int a, int b) {return (a * b);}
-int op_div(int a, int b) {return (a / b);}
-int op_mod(int a, int b) {return (a % b);}
+/**
+*op_add - addition
+* @a : Number
+* @b : Number
+* Return: a + b
+*/
+int op_add(int a, int b)
+	{
+		return (a + b);
+	}
+/**
+* op_sub - subtraction
+* @a : Number
+* @b : Number
+* Return: a - b
+*/
+int op_sub(int a, int b)
+	{
+		return (a - b);
+	}
+/**
+* op_mul - multiplication
+* @a : Number
+* @b : Number
+* Return: a * b
+*/
+int op_mul(int a, int b)
+	{
+		return (a * b);
+	}
+/**
+* op_div - divisions
+* @a : Number
+* @b : Number
+* Return: a / b
+*/
+int op_div(int a, int b)
+	{
+		return (a / b);
+	}
+/**
+* op_mod - modolo
+* @a : Number
+* @b : Number
+* Return: a % b
+*/
+int op_mod(int a, int b)
+	{
+		return (a % b);
+	}
