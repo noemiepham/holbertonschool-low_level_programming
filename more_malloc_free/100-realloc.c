33f4671a3145dccdf74d5c0be7b6318cb58a51d3
noemiepham <huynhtieuquy@gmail.com>
2022-07-01 14:53:08 +0200

    realloc

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -34,6 +34,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		new_size = old_size;
 	for (i = 0; i < new_size; i++)
 		newptr[i] = oldptr[i];
-free(ptr);
+free(newptr);
 return (newptr);
 }
