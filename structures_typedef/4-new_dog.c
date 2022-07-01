81a6e8d1f769176b2dc12332f904b4d258d736a5
noemiepham <huynhtieuquy@gmail.com>
2022-07-01 19:01:06 +0200

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,37 +10,37 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsgined int i, j, k;
+	int i, j, k;
 	dot_t *dog;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	dog = malloc(sizeof(dog_t));
+	dog = malloc(sizeof(*dog));
 	if (dog == NULL)
 		return (NULL);
 	for (i = 0; name[i] != '\0'; i++)
 	;
-	i++;
-	dog->name = malloc(i * sizeof(char));
+	dog->name = malloc(i + 1);
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	for (k = 0; k < i; i++)
-	dog->name[i] = name[i];
+	for (k = 0; k < i; k++)
+	dog->name[k] = name[k];
+	dog->name[k] = '\0';
 	dog->age = age;
 	for (j = 0; owner[j] != '\0'; j++)
 	;
-	j++;
-	dog->owner = malloc(j * sizeof(char));
+	dog->owner = malloc(j + 1);
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	for (k = 0; k < j; i++)
-		dog->owner[k] = owner[i];
+	for (k = 0; k < j; k++)
+		dog->owner[k] = owner[k];
+	dog->owner[k] = '\0';
 	return (dog);
 }
