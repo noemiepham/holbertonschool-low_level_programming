31eb2045678bd0e221b5f7bc8290ad544bb9d0e0
noemiepham <huynhtieuquy@gmail.com>
2022-07-04 10:00:48 +0200

    update variadic_function h

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	if (n > 0)
 	printf("%d", va_arg(list, int));
-	for (i = 0; i < n; i++)
+	for (i = 1; i < n; i++)
 		printf("%s%d", sep, va_arg(list, int));
 	printf("\n");
 	va_end(list);
