5a46cfdbaf85a672c04abc1c0f333ce90f183afb
noemiepham <huynhtieuquy@gmail.com>
2022-07-04 02:46:00 +0200

    variadic_fuctions

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef _VARIADIC_FUCTIONS_H_  /* Include guard */
+#define _VARIADIC_FUCTIONS_H_
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+* variadic_functions.h
+*/
+int sum_them_all(const unsigned int n, ...);
+#endif
