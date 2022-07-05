8e853b6ebef690fe9a2f58adb3cf663727ee5f30
noemiepham <huynhtieuquy@gmail.com>
2022-07-05 15:04:17 +0200

    print 0001 _9899

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - funtion return 0
+*
+* Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	int n;
+	int m;
+
+	n = 0;
+	
+	while (n < 99)
+	{
+		m = n + 1;
+		while (m <= 99)
+			{ 
+				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
+				putchar(' ');
+				putchar((m / 10) + '0');
+				putchar((m % 10) + '0');
+				if (n != 98 && n != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			
+		m++;
+		}
+	n++;
+	}
+putchar('\n');
+return (0);
+}
