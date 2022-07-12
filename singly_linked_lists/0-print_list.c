4bd27e443fc39e62743c6a62076a04a4f3d0532c
noemiepham <huynhtieuquy@gmail.com>
2022-07-12 18:41:12 +0200

    fix code loop infinity

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,16 +8,18 @@
 
 size_t print_list(const list_t *h)
 {
-	int len = 0;
+	int index = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			len++;
+		}
+		index++;
+		h = h->next;
 	}
-	return (len);
+	return (index);
 }
