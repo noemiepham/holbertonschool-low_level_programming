0894c07cbe5496fdc2cd380a6d1768e146407b54
noemiepham <huynhtieuquy@gmail.com>
2022-07-15 16:08:21 +0200

    before main

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -3,8 +3,9 @@
 /**
 * before_main - before program main
 */
-void __attribute__ ((constructor)) before_main() {
-    printf("You're beat! and yet, you must allow,\n");
+void __attribute__ ((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
 
