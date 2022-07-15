034fd20eac47e16f65482aacb4ac87d1af860441
noemiepham <huynhtieuquy@gmail.com>
2022-07-15 17:19:13 +0200

    corriget return

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -11,9 +11,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	/* create a new node */
 	listint_t *new_node =  malloc(sizeof(listint_t));
 
+	if (new_node == NULL)
+		return (NULL);
 	/* Make the newnode points to the head node */
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
-	return (NULL);
+	return (new_node);
 }
