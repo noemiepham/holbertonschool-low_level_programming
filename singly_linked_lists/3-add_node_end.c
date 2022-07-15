1be0604f7dd1b2e06d0c4113c042a3f17b4d2461
noemiepham <huynhtieuquy@gmail.com>
2022-07-15 14:43:49 +0200

    add node end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,7 +1,7 @@
  #include "lists.h"
 #include <stdio.h>
 /**
-* add_node_end - add new node at the end node
+* add_node_end - add new node at the end the la list
 * @head : pointer to head
 * @str : pointer to valeur
 * Return: string len
@@ -9,11 +9,11 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int len = 0;
+	/* create a new node */
+	size_t len = 0;
 	list_t *new_node;
-	list_t *last_node = NULL;
+	list_t *tmp = *head;
 
-	last_node = *head;
 	new_node = malloc(sizeof(list_t)); /* allocating memory */
 	if (new_node == NULL)
 	{
@@ -26,18 +26,20 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = len; /* assigning the value of date */
 	new_node->str = strdup(str);
 	new_node->next = NULL;
-	*head = new_node; /* saving the address of first node */
+	/* if head is NULL, it is an empty list */
 	if (*head == NULL)
-	{
 		*head = new_node;
-		return (new_node);
-	}
-	while (last_node->next != NULL)
+	/*Otherwise, find the last node and add the newNode*/
+	else
 	{
-		last_node = last_node->next;
+		/*last node's next address will be NULL*/
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		/* add the newNode at the end of the linked list */
+		tmp->next = new_node;
 	}
-	last_node->next = new_node;
-	return (new_node);
+	return (tmp);
 }
-
 
