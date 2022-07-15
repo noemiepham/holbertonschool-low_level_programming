5b64f980dafd22bc2edbc1b89de8a58fe4762265
noemiepham <huynhtieuquy@gmail.com>
2022-07-15 17:15:53 +0200

    print list add the begining

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * add_nodeint - function add list head
+ * @head : pointer head NULL
+ * @n :valer int
+ * Return: Null
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	/* create a new node */
+	listint_t *new_node =  malloc(sizeof(listint_t));
+
+	/* Make the newnode points to the head node */
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (NULL);
+}
