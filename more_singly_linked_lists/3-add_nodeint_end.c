29baef8b9d6d2fcdc7eb6356f0bffd20c3cbcaed
noemiepham <huynhtieuquy@gmail.com>
2022-07-16 06:16:22 +0200

    add node last chainee

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* add_nodeint_end - print list number add the end
+* @head: pointer to head list Null
+* @n: valeur type int
+* Return: une list complet
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *tmp = *head;
+
+	new_node->n = n;
+	new_node->next = NULL;
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+			tmp->next = new_node;
+	}
+	return (*head); /* retourn la list complete */
+}
+
+
