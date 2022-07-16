9675c2e3e2fa77b8cdfce65450b0a5d4222e8e30
noemiepham <huynhtieuquy@gmail.com>
2022-07-16 06:22:22 +0200

    return malloc

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,7 +14,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	if (new_node == NULL)
+	{
 		return (NULL);
+	}
 	if (*head == NULL)
 	{
 		*head = new_node;
@@ -27,7 +29,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		}
 			tmp->next = new_node;
 	}
-	return (*head); /* retourn la list complete */
+	return (*head);
+	/* retourn la list complete */
 }
 
 
