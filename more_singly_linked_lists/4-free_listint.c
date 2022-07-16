f3ee5ad2e26f30405c35a4f34d5b6aff63a2d4ce
noemiepham <huynhtieuquy@gmail.com>
2022-07-16 07:07:02 +0200

    free mallo

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - free memory static
+ * @head: pointer to head node
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
