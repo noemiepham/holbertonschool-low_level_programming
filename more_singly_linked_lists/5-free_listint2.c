b847dde0a6eb3368b3a4db2e125bdd35e3d5b9f3
noemiepham <huynhtieuquy@gmail.com>
2022-07-17 11:35:46 +0200

    corrige betty

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,12 +1,16 @@
 #include "lists.h"
+/**
+* free_listint2 - free list
+* @head: pointer to head
+*/
 void free_listint2(listint_t **head)
 {
-    listint_t *tmp;
+	listint_t *tmp;
 
-    while (*head != NULL)
-    {
-        tmp = *head;
-        *head = (*head)->next;
-        free(tmp);
-    }
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
 }
