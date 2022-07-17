e539570361ef59578fa41f3e5a7fee6b5598cb97
noemiepham <huynhtieuquy@gmail.com>
2022-07-17 20:15:42 +0200

    free malloc

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -17,10 +17,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	new_node->n = n;
 	if (new_node == NULL)
-	return (NULL);
+		return (NULL);
 	previous = *head;
 	current = *head;
-
 	if (idx == 0)
 	{
 		new_node->next = *head;
@@ -34,6 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			previous->next = new_node;
 			new_node->next = current;
+			free(new_node);
 			current = NULL; /*arret la boucle */
 		}
 		else
