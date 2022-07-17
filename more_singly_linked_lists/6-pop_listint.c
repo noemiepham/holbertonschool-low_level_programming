e45e917719e992e2e4d2751be69432a3d5587f69
noemiepham <huynhtieuquy@gmail.com>
2022-07-17 11:51:49 +0200

    upload 6-pop_listint

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint - delete node at the begining
+ * @head : pointer to head
+ * Return: current valeur
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *current;
+	int value;
+
+	if (head == NULL)
+		value = 0;
+	else
+	{
+		current = *head;
+		*head = current->next;
+		value = current->n;
+	}
+	return (value);
+}
