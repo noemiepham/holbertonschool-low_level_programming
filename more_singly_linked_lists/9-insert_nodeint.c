a5b83c2a2f8195d984abefa4f21577e2baa7be99
noemiepham <huynhtieuquy@gmail.com>
2022-07-17 20:49:56 +0200

    vérifie allocation node

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -15,9 +15,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count = 0;
 
 	new_node = malloc(sizeof(listint_t));
-	new_node->n = n;
 	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
 	previous = *head;
 	current = *head;
 	if (idx == 0)
