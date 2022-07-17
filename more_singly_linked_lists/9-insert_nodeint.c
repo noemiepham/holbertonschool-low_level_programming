f41ff84b2c46142908fef8f5fcd1c43c7c729530
noemiepham <huynhtieuquy@gmail.com>
2022-07-17 23:27:10 +0200

    insert node

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -9,19 +9,22 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *previous;
 	listint_t *current;
 	listint_t *new_node;
-	listint_t *inserted_node;
 	unsigned int count = 0;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	previous = *head;
-	current = *head;
-	inserted_node = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->next = NULL;
+		return (new_node);
+	}
+
 	if (idx == 0)
 	{
 		new_node->next = *head;
@@ -29,21 +32,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	while (current != NULL)
+	current = *head;
+	while (count < (idx - 1) && current->next != NULL)
 	{
-		if (count == idx)
-		{
-			previous->next = new_node;
-			new_node->next = current;
-			inserted_node = new_node;
-		}
-		else
-		{
-			previous = current;
-			current = current->next;
-		}
-			count++;
+		current = current->next;
+		count++;
 	}
+	if (count < idx - 1)
+		return (NULL);
 
-return (inserted_node);
+	new_node->next = current->next;
+	current->next = new_node;
+
+return (new_node);
 }
