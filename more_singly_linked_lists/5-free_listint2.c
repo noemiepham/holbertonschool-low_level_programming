18d8377f00076ede156974fb9a0c65bca30628ab
noemiepham <huynhtieuquy@gmail.com>
2022-07-17 11:32:12 +0200

    free list nil

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+void free_listint2(listint_t **head)
+{
+    listint_t *tmp;
+
+    while (*head != NULL)
+    {
+        tmp = *head;
+        *head = (*head)->next;
+        free(tmp);
+    }
+}
