e91593482cda919a0ce4e02473363367fc7220a1
noemiepham <huynhtieuquy@gmail.com>
2022-07-18 09:32:20 +0200

    condition binay == NULL

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -14,12 +14,12 @@ unsigned int binary_to_uint(const char *b)
 	int c;
 	unsigned int len = 0;
 
+	if (b == NULL)
+		return (0);
 	while (b[len])
 	{
 		len++;
 	}
-	if (b == NULL)
-		return (0);
 	for (c = len - 1; c >= 0; c--)
 	{
 		if (b[c] != '0' && b[c] != '1')
