1e61a9abe4fb2a3601eb0fe68e7f5adafc21922d
noemiepham <huynhtieuquy@gmail.com>
2022-07-18 13:05:38 +0200

    print binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - print binary number
+ * @n : number to convert
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int i;
+
+	if (n > 1)
+	print_binary(n >> 1);
+
+	printf("%lu", (n & 1));
+}
