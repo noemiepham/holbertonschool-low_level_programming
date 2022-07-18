4c5550a7f0f782a6dee48f1d0c1346a460f2dce6
noemiepham <huynhtieuquy@gmail.com>
2022-07-18 09:23:30 +0200

    convert binary

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "strings.h"
+#include "main.h"
+
+/**
+ * binary_to_uint - convert to binay base 2
+ * @b: pointer to string number
+ * Return: numero convert
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int k = 1;
+	unsigned int i = 0;
+	int c;
+	unsigned int len;
+
+	len = strlen(b);
+	for (c = len - 1; c >= 0; c--)
+	{
+		if (b[c] != '0' && b[c] != '1')
+		return (0);
+		if (b[c] == '1')
+		{
+			i += k;
+		}
+		k *= 2;
+	}
+	return (i);
+}
