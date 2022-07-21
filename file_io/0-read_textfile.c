739cd443aaf73f75af31f6507858abbf7ef1d845
noemiepham <huynhtieuquy@gmail.com>
2022-07-21 13:26:25 +0200

    condition

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -16,22 +16,20 @@ size_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
+
+	fl = open(filename, O_RDONLY);
 	if (fl == -1)
 		return (0);
 
-	fl = open(filename, O_RDONLY);
-	buf = malloc(sizeof(char) * letters + 1);
+	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 		return (0);
 
 	rd = read(fl, buf, letters);
-	if (rd == -1)
-		return (0);
-	buf[letters] = '\0';
 
-	wr = write(1, buf, rd);
-	if (wr == -1)
-		return (0);
+	wr = write(STDOUT_FILENO, buf, rd);
+	/* stdout_lfileo: descriptor of linux system */
+
 	close(fl);
 	free(buf);
 	return (wr);
