99a5c267e1af122d471e7dc6e9ec2823c3b421d1
noemiepham <huynhtieuquy@gmail.com>
2022-07-21 13:32:53 +0200

    corriget faute

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,23 +9,20 @@ int create_file(const char *filename, char *text_content)
 {
 	int fl;
 	int len = 0;
+	int wr;
 
 	if (filename == NULL)
 		return (-1);
-	fl = open(filename, O_CREAT | O_TRUNC | O_RDONLY, 0600);
+	fl = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fl == -1)
 		return (-1);
 	if (text_content == NULL)
-	{
-		close(fl);
-		return (-1);
-	}
+		text_content = "";
 	while (text_content[len])
-	{
 		len++;
-	}
-
-	write(fl, text_content, len);
+	wr = write(fl, text_content, len);
+	if (wr == -1)
+		return (-1);
 	close(fl);
 	return (1);
 }
