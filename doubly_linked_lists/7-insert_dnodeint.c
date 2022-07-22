9d0d219adc2452a8419cf209c842176332b489ac
noemiepham <huynhtieuquy@gmail.com>
2022-07-22 06:24:14 +0200

    corriger error current->next nUll

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,37 +15,38 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp;
 
 	new_node = malloc(sizeof(dlistint_t));
+	current = *h;
 	if (new_node == NULL)
-	return (NULL);
+		return (NULL);
 	new_node->n = n;
-
-	if (*h == NULL)
+	if (*h == NULL || h == NULL)
 	{
 		*h = new_node;
 		new_node->next = NULL;
 		return (new_node);
 	}
-
 	if (idx == 0)
 	{
 		new_node->next = *h;
 		*h = new_node;
-		free(new_node);
-		return (new_node);
 	}
-
-	current = *h;
-	while (i < (idx - 1) && current->next != NULL)
+	else
 	{
-		current = current->next;
-		i++;
+		while (i < (idx - 1))
+		{
+			if (current->next == NULL)
+			{
+				new_node->next = NULL;
+				current->prev = new_node;
+			}
+			current = current->next;
+			i++;
+		}
+		tmp = current->next;
+		new_node->next = current->next;
+		new_node->prev = current;
+		current->next = new_node;
+		tmp->prev = new_node;
 	}
-	if (i < idx - 1)
-		return (NULL);
-	tmp = current->next;
-	new_node->next = current->next;
-	new_node->prev = current;
-	current->next = new_node;
-	tmp->prev = new_node;
 	return (new_node);
 }
