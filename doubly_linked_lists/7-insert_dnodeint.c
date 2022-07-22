62346d20b0449510f0bad78930cd613387e0b8ad
noemiepham <huynhtieuquy@gmail.com>
2022-07-22 06:55:27 +0200

    correct insert one in list 2 element

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,16 +14,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current;
 
 	new_node = malloc(sizeof(dlistint_t));
-	current = *h;
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
-	if (*h == NULL || h == NULL)
-	{
-		*h = new_node;
-		new_node->next = NULL;
-		return (new_node);
-	}
+	current = *h;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
 	while (i < (idx - 1))
 	{
 		current = current->next;
@@ -31,14 +30,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		i++;
 	}
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	else
-	{
-		new_node->prev = current;
-		new_node->next = current->next;
-		current->next->prev = new_node;
-		current->next = new_node;
-	}
+	if (new_node->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new_node->prev = current;
+	new_node->next = current->next;
+	current->next->prev = new_node;
+	current->next = new_node;
 	return (new_node);
 }
