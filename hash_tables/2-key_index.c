5f30384ee5fa87f8b40be1e5060c4eb4ab0688ee
noemiepham <huynhtieuquy@gmail.com>
2022-08-21 00:20:00 +0200

    index de key

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * key_index - calcul key index
+ * @key: to be get index
+ * @size: size of the hash table
+ *
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key));
+}
