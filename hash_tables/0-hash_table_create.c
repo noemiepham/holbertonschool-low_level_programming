28d094cf94e9441378eec0f00fc65d87e27b8794
Noémiepham <huynhtieuquy@gmail.com>
2022-08-20 14:29:45 -0700

    16400 char long erro

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *));
+	table->array = malloc(size * sizeof(hash_node_t *));
 
 	if (table->array == NULL)
 	{
@@ -27,4 +27,3 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	return (table);
 }
-
