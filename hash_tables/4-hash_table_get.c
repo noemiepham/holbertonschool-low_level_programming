1ae6b7f996d830b587e752a02c88135af3d7ca1b
noemiepham <huynhtieuquy@gmail.com>
2022-08-21 21:55:03 +0200

    print table

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -8,21 +8,21 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index;
-	hash_node_t *node;
+        unsigned long int index;
+        hash_node_t *node;
 
-	if (ht == NULL || key == NULL)
-		return (NULL);
+        if (ht == NULL || key == NULL)
+                return (NULL);
 
-	index = key_index((const unsigned char *)key, ht_size);
-	node = ht->array[index];
+        index = key_index((const unsigned char *)key, ht->size);
+        node = ht->array[index];
 
-	while (node != NULL)
-	{
-		if (strcmp(key, node->key) == 0)
-			return (node);
+        while (node != NULL)
+        {
+                if (strcmp(key, node->key) == 0)
+                        return (node->value);
 
-		node = node->next;
-	}
-	return (NULL);
+                node = node->next;
+        }
+        return (NULL);
 }
