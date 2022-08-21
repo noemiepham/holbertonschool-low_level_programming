f523740e51d9c9236c4d9a79df771cc318b686a0
noemiepham <huynhtieuquy@gmail.com>
2022-08-21 21:30:14 +0200

    error compile

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,13 +7,14 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned int i = 0;
+	unsigned long int i = 0;
+	unsigned long int i;
 	hash_node_t *node;
 
 	if (ht == NULL)
 		return (NULL);
 	printf("{");
-	for (i = 0; i < ht.size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[index];
 		while (node != NULL)
