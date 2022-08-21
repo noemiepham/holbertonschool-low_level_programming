d9ad5d9a68913dcbfcfe96aaa9c971e27adaca8e
Noémiepham <huynhtieuquy@gmail.com>
2022-08-20 23:29:47 -0700

    erro complie

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <string.h>
 /**
  * hash_table_set - a function that adds an element to the hash table.
  * @ht: is the hash table you want to add or update the key/value to.
@@ -9,18 +10,16 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node, *tmp;
-	unsigned long int idx;
-	char *str;
+	unsigned long int index;
 
 	if (ht == NULL || key == NULL)
 		return (0);
 
-	idx = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 	while (tmp)
 	{
-		if (strcmp(tmp->value))
-			;
+		if (strcmp(tmp->key, key) == 0)
 		{
 			tmp->value = strdup(value);
 			if (tmp->value == NULL)
