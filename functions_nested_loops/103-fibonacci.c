1a198374cb4fa5a586898f1acebcf4e393aaf2cd
noemiepham <huynhtieuquy@gmail.com>
2022-08-31 15:42:29 +0200

    finbonacci

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print sum evan fibonacci
+ * Return: non void
+ *
+ */
+
+int main(void)
+{
+	unsigned long count, i, j, k, sum;
+
+	i = 0;
+	j = 1;
+	for (count = 0; count < 50; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		if (k % 2 == 0 && k < 4000000)
+		{
+			sum += k;
+		}
+	}
+	printf("%lu\n", k);
+	return (0);
+}
