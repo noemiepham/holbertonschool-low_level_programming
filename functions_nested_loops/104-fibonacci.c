7df441a2217207ece4517e8a3ccc3accc59b7a92
noemiepham <huynhtieuquy@gmail.com>
2022-08-31 16:09:16 +0200

    fibonacci next

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - the 98 finbonacci
+ * Return: non void
+ */
+
+int main(void)
+{
+	int count = 0;
+	unsigned long i, j, k;
+	unsigned long m, n, carry, p;
+
+	j = 1;
+	i = 0;
+	for (count = 1; count <= 91; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu, ", k);
+	}
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
+	{
+		carry = (m + n) / 1000;
+		p = (m + n) - carry * 1000;
+		k = (i + j) + carry;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			printf("%lu%lu", k, p);
+		else
+			printf("%lu0%lu", k, p);
+		if (count != 98)
+			printf(", ");
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}
