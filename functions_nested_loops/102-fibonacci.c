d19356f55db748245ba7fc8ea9cd2a11d92be0f7
noemiepham <huynhtieuquy@gmail.com>
2022-08-31 14:29:30 +0200

    fibonacci

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - program to print 5à Fibonacci
+ * Return: non void return
+ */
+
+int main(void)
+{
+	int count, i, j, k;
+
+	i = 0;
+	j = 1;
+	for (count = 0; count < 50; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%u", k);
+		if (count == 49)
+			putchar('\n');
+		else
+			printf(", ");
+	}
+	return (0);
+}
