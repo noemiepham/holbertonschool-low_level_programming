a18246c56f78e3953286d92bb1d0566f4e639f1c
noemiepham <huynhtieuquy@gmail.com>
2022-09-01 09:32:02 +0200

    prime factor

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prime factor
+ * Return: non void
+ */
+int main(void)
+{
+	long int n = 612852475143;
+	long int i, k;
+
+	for (i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			n /= i;
+		}
+	}
+	printf("%li\n", n);
+	return (0);
+}
