c3e87702c2e98baf14e1937bf7245846aa0cc9da
noemiepham <huynhtieuquy@gmail.com>
2022-09-01 10:28:55 +0200

    print number

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number - print integer
+ * @n: number to print
+ * Return: non void
+ */
+void print_number(int n)
+{
+	int i = 48;
+
+	if (n < 0)
+	{
+		_putchar(45);
+	}
+	if (n < -9)
+	{
+		print_number(n / -10);
+		_putchar(i - n % 10);
+	}
+	else
+	{
+		if (n > 9)
+		{
+			print_number(n / 10);
+		}
+		_putchar(n % 10 + i);
+	}
+}
